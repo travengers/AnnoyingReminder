3796a402a52e38cbd79c9cd360ecbaaaaaf47273
Cahaya Ikhwan <cikwanputra@gmail.com>
2015-10-19 14:29:47 +0700

    Phase Two : +++ bisa di drag, kalo dilepas ilang

--- app/src/main/java/com/example/cikwanp/annoyingreminder/PopupService.java
+++ app/src/main/java/com/example/cikwanp/annoyingreminder/PopupService.java
@@ -17,6 +17,13 @@ public class PopupService extends Service {
     private WindowManager windowManager;
     private ImageView chatHead;
     ScreenReceiver receiver = new ScreenReceiver();
+    private boolean isMove;
+    final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.TYPE_PHONE,
+            WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+            PixelFormat.TRANSLUCENT);
 
     @Override
     public void onCreate() {
@@ -30,12 +37,6 @@ public class PopupService extends Service {
         chatHead = new ImageView(this);
         chatHead.setImageResource(R.drawable.popups);
 
-        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.TYPE_PHONE,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                PixelFormat.TRANSLUCENT);
 
         params.gravity = Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL;
         params.x = 0;
@@ -51,9 +52,41 @@ public class PopupService extends Service {
         chatHead.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                chatHead.animate().alpha(0.2f).scaleX(0.1f).scaleY(0.1f);
-                windowManager.removeView(chatHead);
-                stopSelf();
+                if(isMove==false){
+                    chatHead.animate().alpha(0.2f).scaleX(0.1f).scaleY(0.1f);
+                    windowManager.removeView(chatHead);
+                    stopSelf();
+                }
+
+            }
+        });
+
+
+        chatHead.setOnTouchListener(new View.OnTouchListener() {
+            private int initialX;
+            private int initialY;
+            private float initialTouchX;
+            private float initialTouchY;
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        initialX = params.x;
+                        initialY = params.y;
+                        initialTouchX = event.getRawX();
+                        initialTouchY = event.getRawY();
+                        return isMove;
+                    case MotionEvent.ACTION_UP:
+                        return isMove = false;
+                    case MotionEvent.ACTION_MOVE:
+                        params.x = initialX + (int) (event.getRawX() - initialTouchX);
+                        params.y = initialY + (int) (event.getRawY() - initialTouchY);
+                        windowManager.updateViewLayout(chatHead, params);
+                        return true;
+
+                }
+                return false;
             }
         });
 
